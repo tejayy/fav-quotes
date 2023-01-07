707cc9c552ee7e77d00c60a6070f5c7802b8df95
star <tbj456@gmail.com>
2023-01-07 14:06:44 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+Web: gunicorn quotes:app
---
+++ quotes.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:root@localhost/quotes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:a4rVRo7a9i6mlLcosHgO@containers-us-west-59.railway.app:7469/railway'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
+
+class Favquotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    quote = db.Column(db.String(2000))
+
+
+@app.route('/')
+def index():
+    result = Favquotes.query.all()
+    return render_template('index.html', result=result)
+
+
+@app.route('/quotes')
+def quotes():
+    return render_template('quotes.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    author = request.form['author']
+    quote = request.form['quote']
+    quotedata = Favquotes(author=author, quote=quote)
+    db.session.add(quotedata)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
